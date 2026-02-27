773ba2a0ec30346676a955b494528cdfcb749763
Kareena Lakhani <kareenalakhani@Mac.lan>
2026-02-27 15:12:07 -0700

    Configure Astro for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://Kareena2207.github.io",
+  base: "/kareena-portfolio",
+
   vite: {
     plugins: [tailwindcss()]
   },
 
   integrations: [mdx(), sitemap()]
 });
+
